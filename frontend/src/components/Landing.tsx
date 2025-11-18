2bd6b0a37c661d4cfd9f151c3fa2154e64f73963
anurrraggg <anuragpandey945028@gmail.com>
2025-11-18 22:59:21 +0530

    se

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 export const Landing = () => {
   const [name, setName] = useState('');
@@ -8,12 +9,12 @@ export const Landing = () => {
         setName(e.target.value);
     }}>
     </input>
-    <button onClick={() => {
+    <Link to={`/room?name=${name}`} onClick={() => {
 
     // room join logic 
 
 
-    }}> JOIN </button>
+    }}> JOIN </Link>
 </div>
   );
 }
